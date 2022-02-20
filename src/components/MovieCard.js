0da1dbfc1f657c8ec4fa2be9ca048388b1d38180
William Yu <wy427@nyu.edu>
2022-02-20 01:16:27 -0500

    assignment completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+//destructured props
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
